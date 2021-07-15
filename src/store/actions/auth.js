656536209b067768907098fa28733f3018a7516d
neutroline-sushant <sushant.neutroline@gmail.com>
2021-07-15 10:11:52 +0545

    redux

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,4 +1,5 @@
 import * as actionTypes from './actionTypes';
+import axios from 'axios';
 
 export const authStart = () =>{
     return {
@@ -23,5 +24,18 @@ export const authFail = (error) =>{
 export const auth = (email, password) =>{
     return dispatch =>{
         dispatch(authStart());
+        const authData = {
+            email: email,
+            password: password
+        };
+        axios.post('http://localhost:4000/user/login', authData)
+        .then(response => {
+            console.log(response);
+            dispatch(authSuccess(response.data));
+        })
+        .catch(error =>{
+            console.log(error);
+            dispatch(authFail(error));
+        })
     };
 };
