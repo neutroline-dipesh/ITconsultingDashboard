23adff6cb6842e99d84b026b343921053a49505b
DipeshShresth <Dipesh.neutroline@gmail.com>
2021-07-16 10:50:26 +0545

    change

--- src/App.js
+++ src/App.js
@@ -16,30 +16,30 @@ import viewApplicantDetail from "./components/viewApplicantDetail/viewApplicantD
 import viewJobDetail from "./components/All jobs/viewJobDetail";
 function App() {
   return (
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={Login} />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Login} />
 
-          <Route exact path="/dashboard" component={Dashboard} />
-          <Route exact path="/alljobs" component={Alljobs} />
-          <Route exact path="/alljobs/editJobs" component={EditJobs} />
-          <Route exact path="/contracting" component={Contracting} />
-          <Route exact path="/internal" component={Internal} />
-          <Route exact path="/addjobs" component={AddJobs} />
-          <Route exact path="/contact" component={Contact} />
-          <Route exact path="/viewContact" component={viewDetail} />
-          <Route exact path="/allApplicant" component={allApplicant} />
-          <Route exact path="/viewJobDetail" component={viewJobDetail} />
+        <Route exact path="/dashboard" component={Dashboard} />
+        <Route exact path="/alljobs" component={Alljobs} />
+        <Route exact path="/alljobs/editJobs" component={EditJobs} />
+        <Route exact path="/contracting" component={Contracting} />
+        <Route exact path="/internal" component={Internal} />
+        <Route exact path="/addjobs" component={AddJobs} />
+        <Route exact path="/contact" component={Contact} />
+        <Route exact path="/viewContact" component={viewDetail} />
+        <Route exact path="/allApplicant" component={allApplicant} />
+        <Route exact path="/viewJobDetail" component={viewJobDetail} />
 
-          <Route
-            exact
-            path="/viewApplicatnDetail"
-            component={viewApplicantDetail}
-          />
+        <Route
+          exact
+          path="/viewApplicatnDetail"
+          component={viewApplicantDetail}
+        />
 
-          <Route component={pageNotFound} />
-        </Switch>
-      </BrowserRouter>
+        <Route component={pageNotFound} />
+      </Switch>
+    </BrowserRouter>
     // <div>
     //   {/* <Login /> */}
     //   {/* <Sidebar /> */}
--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,41 +1,42 @@
-import * as actionTypes from './actionTypes';
-import axios from 'axios';
+import * as actionTypes from "./actionTypes";
+import axios from "axios";
 
-export const authStart = () =>{
-    return {
-        type: actionTypes.AUTH_START
-    };
+export const authStart = () => {
+  return {
+    type: actionTypes.AUTH_START,
+  };
 };
 
-export const authSuccess = (authData) =>{
-    return{
-        type: actionTypes.AUTH_SUCCESS,
-        authData: authData
-    };
+export const authSuccess = (authData) => {
+  return {
+    type: actionTypes.AUTH_SUCCESS,
+    authData: authData,
+  };
 };
 
-export const authFail = (error) =>{
-    return{
-        type: actionTypes.AUTH_FAIL,
-        error: error
-    };
+export const authFail = (error) => {
+  return {
+    type: actionTypes.AUTH_FAIL,
+    error: error,
+  };
 };
 
-export const auth = (email, password) =>{
-    return dispatch =>{
-        dispatch(authStart());
-        const authData = {
-            email: email,
-            password: password
-        };
-        axios.post('http://localhost:4000/user/login', authData)
-        .then(response => {
-            console.log(response);
-            dispatch(authSuccess(response.data));
-        })
-        .catch(error =>{
-            console.log(error);
-            dispatch(authFail(error));
-        })
+export const auth = (email, password) => {
+  return (dispatch) => {
+    dispatch(authStart());
+    const authData = {
+      email: email,
+      password: password,
     };
+    axios
+      .post("http://localhost:4000/user/login", authData)
+      .then((response) => {
+        console.log(response);
+        dispatch(authSuccess(response.data));
+      })
+      .catch((error) => {
+        console.log(error);
+        dispatch(authFail(error));
+      });
+  };
 };
