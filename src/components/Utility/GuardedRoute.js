3cfcc8148a500fbee69e54e1057d7323c93302e4
neutroline-sushant <sushant.neutroline@gmail.com>
2021-08-19 18:59:51 +0545

    routing fix

--- src/components/Utility/GuardedRoute.js
+++ src/components/Utility/GuardedRoute.js
@@ -1,13 +1,19 @@
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
 
-const GuardedRoute = ({ component: Component, auth, ...rest }) => (
+const GuardedRoute = ({ component: Component, auth, ...rest }) => {
+  console.log("Component"+Component);
+  console.log("Auth"+ auth);
+  console.log(rest); 
+  return(
   <Route
     {...rest}
+    exact
     render={(props) =>
       auth === true ? <Component {...props} /> : <Redirect to="/" />
     }
   />
-);
+  )
+  };
 
 export default GuardedRoute;
